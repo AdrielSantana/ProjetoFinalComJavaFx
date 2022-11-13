c9b621e93d0ed8343ee5345b2d3c94795a686aa6
CauanS29 <cauansantos043@gmail.com>
2022-11-12 23:47:45 -0300

    finalizando as classes de teste

---
+++ ProjetoLivraria/src/livraria/backend/teste/EstoqueDeLivroTest.java
@@ -0,0 +1,26 @@
+package livraria.backend.teste;
+
+import livraria.backend.Autor;
+import livraria.backend.EstoqueDeLivro;
+import livraria.backend.produtos.LivroFisico;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EstoqueDeLivroTest {
+
+    @Test
+    public void achaLivro(){
+        Autor autor = new Autor();
+        autor.setNome("Mauricio Aniche");
+        LivroFisico fisico = new LivroFisico(autor);
+        fisico.setNome("Javinha");
+        fisico.setValor(36.5);
+        fisico.setQuantidade(5);
+        EstoqueDeLivro livros = new EstoqueDeLivro();
+        livros.adicionaLivro("Javinha",fisico);
+
+        assertEquals(fisico,livros.achaLivro("Javinha"));
+    }
+
+}
